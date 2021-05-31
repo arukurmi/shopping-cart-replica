54c8a723dd305a698d6c8de1606a1e09136b537f
Aryansh <2018eeb1141@iitrpr.ac.in>
2021-05-31 16:20:40 +0530

    first commit

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,31 @@
 import React, { Component } from 'react'
 
 export default class Counter extends Component {
+    state = {
+        count: 0
+    };
+
+    styles = {
+        fontSize: 30,
+        fontWeight: "bold"
+    };
+
     render() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+
         return (
-            <h1>Aryansh</h1>
-        )
+            <React.Fragment>
+                <span style={this.styles} className={classes}>
+                    {this.formatCount()}
+                </span>
+                <button className="btn btn-secondary btn-sm">+Incse+</button>
+            </React.Fragment>
+        );
+    }
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? 'Zeroooooo' : count;
+
     }
 }
