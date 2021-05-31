3713300246a48d97f01c9f1a209bf19e69fe54c9
Aryansh <2018eeb1141@iitrpr.ac.in>
2021-05-31 12:51:20 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -4,15 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
+import Counter from './components/counter';
 
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+ReactDOM.render(<Counter/> , document.getElementById("root"));
 
 
 reportWebVitals();
